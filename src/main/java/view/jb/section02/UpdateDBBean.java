936c6bfbc46ed1507ef8bcd487980764f84ee7ee
Flulu_Desk <gitmiu@gmail.com>
2020-12-24 09:52:23 +0900

    hashing pwd

--- src/main/java/view/jb/section02/UpdateDBBean.java
+++ src/main/java/view/jb/section02/UpdateDBBean.java
@@ -1,14 +1,25 @@
 package view.jb.section02;
 
+//connection pool
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
+
+//sql
+import java.nio.charset.StandardCharsets;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+
+//collection framework
 import java.util.ArrayList;
 import java.util.List;
+
+
+//hashing
+import view.jb.hash.SHA256;
+import view.jb.hash.BCrypt;
 
 public class UpdateDBBean {
 
@@ -88,5 +99,59 @@ public class UpdateDBBean {
                 PreparedStatement pstmt = null;
                 ResultSet rs = null;
 
+                //get SHA-256 Object
+                SHA256 sha = SHA256.getInsatnce();
+
+                try {
+                    conn = getConnection();
+
+                    pstmt = conn.prepareStatement("SELECT ID, PWD FROM MEMBER");
+                    rs = pstmt.executeQuery();
+
+                    while (rs.next()) {
+                        String id = rs.getString("ID");
+                        String pwd = rs.getString("PWD");
+
+                        //current password -> hashing password
+                        String shaPWD = sha.getSha256(pwd.getBytes());
+
+                        //sha256 pwd -> BCrypt hash
+                        String bcPWD = BCrypt.hashpw(shaPWD, BCrypt.gensalt());
+
+                        pstmt = conn.prepareStatement("UPDATE MEMBER SET PWD= ? WHERE ID = ?");
+                        pstmt.setString(1, bcPWD);
+                        pstmt.setString(2, id);
+                        pstmt.executeUpdate();
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }finally {
+                    if (rs != null) {
+                        try {
+                            rs.close();
+                        } catch (SQLException sqlException) {
+                            sqlException.printStackTrace();
+                        }
+                    }
+
+                    if (pstmt != null) {
+                        try {
+                            pstmt.close();
+                        } catch (SQLException sqlException) {
+                            sqlException.printStackTrace();
+                        }
+
+                    }
+
+                    if (conn != null) {
+                        try {
+                            conn.close();
+                        } catch (SQLException sqlException) {
+                            sqlException.printStackTrace();
+                        }
+
+                    }
+                }
             }
 }
