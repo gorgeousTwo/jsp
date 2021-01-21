b6c183db942b5fcdd6eaae2d7b9805de79fb60b6
GaraFaLnad_D <gitmiu@gmail.com>
2021-01-21 10:16:07 +0900

    review : lpb again again again

---
+++ src/main/java/review/jb/db/ldb.java
@@ -0,0 +1,49 @@
+package review.jb.db;
+
+public class ldb {
+    private String id;
+    private String pwd;
+    private String name;
+    private String address;
+    private String tel;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+}
