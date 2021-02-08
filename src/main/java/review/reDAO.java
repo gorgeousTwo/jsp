9cce06bf30fc7ddeca5403c64603f1c79949820a
GaraFaLnad_D <gitmiu@gmail.com>
2021-02-08 09:34:31 +0900

    review

--- src/main/java/review/reDAO.java
+++ src/main/java/review/reDAO.java
@@ -5,6 +5,7 @@ import javax.naming.InitialContext;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class reDAO {
     private Connection conn = null;
@@ -35,6 +36,52 @@ public class reDAO {
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return -1;
+    }
+
+    public int idCheck(String id) {
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            pstmt = conn.prepareStatement("SELECT ID FROM REVIEWF WHERE ID =?");
+            pstmt.setString(1, id);
+            rs = pstmt.executeQuery();
+
+            if (!rs.next()) {
+                return 1;
+            }
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+    public int updateMember(reDTO reDTO) {
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        reDTO re = null;
+
+        try {
+            pstmt = conn.prepareStatement("SELECT PWD FROM REVIEWF WHERE ID = ?");
+            pstmt.setString(1, reDTO.getReName());
+            rs = pstmt.executeQuery();
+
+            if (reDTO.getRePwd() == rs.getString("PWD")) {
+                pstmt = conn.prepareStatement("UPDATE REVIEWF SET NAME=?, PWD=?, TEL =?");
+                pstmt.setString(1,reDTO.getReName());
+                pstmt.setString(2,reDTO.getRePwd());
+                pstmt.setString(3,reDTO.getReTel());
+                pstmt.executeUpdate();
+                return 1;
+            }else{
+                return 0;
+            }
+
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+
         return -1;
     }
 }
